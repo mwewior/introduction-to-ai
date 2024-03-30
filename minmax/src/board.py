9b240c7e2df0294966d220fda14e5f1e08a913fd
mwewior <01168918@pw.edu.pl>
2024-03-30 20:29:51 +0100

    working human vs human

--- minmax/src/board.py
+++ minmax/src/board.py
@@ -1,3 +1,7 @@
+class SquareOccupiedError(Exception):
+    pass
+
+
 def printable_row_col(row, col):
     print_row = row*3 + 1
     print_col = col*(5+1) + 2
@@ -14,7 +18,6 @@ class Player:
 
 
 class Board:
-
     def create_board(self, size: int) -> list:
         plain_board = []
         for r in range(size):
@@ -44,7 +47,7 @@ class Board:
                 num = self._simple_board[row][col]
                 print_row, print_col = printable_row_col(row, col)
                 if num < 10:
-                    plain_board[print_row][print_col] = num
+                    plain_board[print_row][print_col] = str(num)
                 else:
                     plain_board[print_row][print_col] = str(num)[0]
                     plain_board[print_row][print_col + 1] = str(num)[1]
@@ -58,12 +61,16 @@ class Board:
         self._x_player = True
         self._simple_board = self.create_board(self._size)
         self._print_board = self.create_print_board(self._size)
+        self._is_finished = False
 
     def board(self) -> list:
         return self._simple_board
 
     def get_print_board(self) -> list:
         return self._print_board
+
+    def finished(self):
+        return self._is_finished
 
     def round(self) -> int:
         return self._round_count
@@ -81,15 +88,33 @@ class Board:
             player = self._players[1]
         return player
 
-    def read_input(self):
-        self.next_player()
-        player = self.get_char_player()
-        print(f'\nCurrent player is {player}')
-        position = input("Choose square:\n")
+    def convert_input(self, position: int):
         row = (int(position)-1) // self._size  # całkowitoliczbowe
         col = int(position) % self._size - 1   # modulo
         if col == -1:
             col = self._size-1
+        return row, col
+
+    def validate_input(self):
+        position = input("Choose square:\n")
+        try:
+            position = int(position)
+            row, col = self.convert_input(position)
+            if type(self._simple_board[row][col]) is str:
+                raise SquareOccupiedError
+        except ValueError and IndexError:
+            print(f'\nValue must be a integer between 1 and {self._max_round}')
+            position = self.validate_input()
+        except SquareOccupiedError:
+            print('Cannot put your mark on occupied square')
+            position = self.validate_input()
+        return position
+
+    def read_input(self):
+        player = self.get_char_player()
+        print(f'\nCurrent player is {player}')
+        position = self.validate_input()
+        row, col = self.convert_input(position)
         return row, col
 
     def print(self):
@@ -107,7 +132,34 @@ class Board:
         board[print_row][print_col] = self._simple_board[row][col]
         board[print_row][print_col + 1] = ' '
 
+    def check_win(self):
+        for i in range(self._size):
+            row_count = 0
+            col_count = 0
+            diag_up_count = 0
+            diag_down_count = 0
+
+            for j in range(self._size):
+                if self._simple_board[i][j] == self._simple_board[i][0]:
+                    row_count += 1
+                if self._simple_board[j][i] == self._simple_board[0][i]:
+                    col_count += 1
+                if self._simple_board[j][j] == self._simple_board[0][0]:
+                    diag_up_count += 1
+                if self._simple_board[-(j+1)][j] == self._simple_board[0][-1]:
+                    diag_down_count += 1
+
+            if row_count == self._size:
+                self._is_finished = True
+            if col_count == self._size:
+                self._is_finished = True
+            if diag_up_count == self._size:
+                self._is_finished = True
+            if diag_down_count == self._size:
+                self._is_finished = True
+
     def move(self):
+        self.next_player()
         row, col = self.read_input()
         self._simple_board[row][col] = self.get_char_player()
         self.update_print_board(row, col)
@@ -117,9 +169,18 @@ class Board:
 
 board = Board(3)
 board.print()
-while board.round() < board.max_round():
+while board.round() < board.max_round() and not board.finished():
     board.move()
-
+    if board.round() >= 5:
+        board.check_win()
+if board._x_player:
+    won_player = board._players[0]
+else:
+    won_player = board._players[1]
+if board.finished():
+    print(f"\n\n'{won_player}' won the game!\n")
+else:
+    print('\n\nDraw!\n')
 
 
 
