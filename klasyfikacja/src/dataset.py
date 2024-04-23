b18cba42f3dc3112b8d3065245bcc4df106c2b6f
mwewior <01168918@pw.edu.pl>
2024-04-23 22:24:45 +0200

    schema of program body

--- klasyfikacja/src/dataset.py
+++ klasyfikacja/src/dataset.py
@@ -1,7 +1,13 @@
+import copy
 from ucimlrepo import fetch_ucirepo
+
 # import pandas as pd
 import numpy as np
+
 np.random.seed(318407)
+
+
+K = 5
 
 
 iris = fetch_ucirepo(id=53)
@@ -12,7 +18,26 @@ feature = dataset.features
 target = dataset.targets
 
 shuffled = original.sample(frac=1)
-grouped_sets = np.array_split(shuffled, 5)
+grouped_sets = np.array_split(shuffled, K + 1)
 
-validation_group = grouped_sets.pop(-1)
-training_group = grouped_sets
+test_group = grouped_sets.pop(-1)
+cross_group = grouped_sets
+
+
+def learning():
+    # tutaj to jakoś się uczy, daje parametry potem
+    return 0
+
+
+def testing():
+    # dla uzyskanych parametrów klasyfikacji przeprowadzamy
+    # badanie na danych testowych i oceniamy jakość
+    return 0
+
+
+parameters = []
+for i in range(K):
+    train_group = copy.deepcopy(cross_group)
+    validation_group = train_group.pop(K)
+    parameters.append(learning())
+test_results = testing()
