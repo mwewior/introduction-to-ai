e8b99b8816dca4d37cc792d27d87e1eb2fe78cb9
mwewior <01168918@pw.edu.pl>
2024-04-27 12:44:19 +0200

    jakieś wstępne testy, ale jeszcze trzeba crossvalidacje zrobić

--- klasyfikacja/src/classifier.py
+++ klasyfikacja/src/classifier.py
@@ -1,6 +1,6 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import cross_val_score
 
 from dataset import DataSet
 
@@ -10,7 +10,7 @@ clfTree = DecisionTreeClassifier(
 )
 
 clfSVM = SVC(
-    C=1, kernel="linear", tol=10e-16  # , max_iter=int(10e4)
+    C=1, kernel="poly", tol=10e-16  # , max_iter=int(10e4)
 )
 
 ds = DataSet(K=5)
@@ -18,14 +18,35 @@ ds = DataSet(K=5)
 trainFeatures = ds.joinedTrainFeatures
 trainTarget = ds.joinedTrainTarget
 
-scoresTree = cross_val_score(clfTree, trainFeatures, trainTarget, cv=5)
-scoresSVM = cross_val_score(clfSVM, trainFeatures, trainTarget, cv=5)
-print(f"\nDecision Tree: {scoresTree}\n{10*' '}SVM: {scoresSVM}\n")
+# scoresTree = cross_val_score(clfTree, trainFeatures, trainTarget, cv=5)
+# scoresSVM = cross_val_score(clfSVM, trainFeatures, trainTarget, cv=5)
+# print(f"\nDecision Tree: {scoresTree}\n{10*' '}SVM: {scoresSVM}\n")
 
-clfSVM.fit(trainFeatures, trainTarget)
+singleGroupLength = len(trainTarget)//5
 
-test_prediction = clfSVM.predict(ds.testFeatures)
-testTarget = ds.testTarget
+# print(len(trainTarget))
+# print(singleGroupLength)
+# print(trainFeatures)
+# print(trainTarget)
 
-for i in range(len(testTarget)):
-    print(f'{test_prediction[i]} : {testTarget[i]}')
+print("prefit\n")
+
+clfSVM.fit(
+    trainFeatures[0:4*singleGroupLength],
+    trainTarget[0:4*singleGroupLength])
+
+print("\n\nvalidation\n")
+testPrediction = clfSVM.predict(ds.testFeatures)
+expectedTarget = ds.testTarget
+
+for i in range(len(expectedTarget)):
+    diff = testPrediction[i] - expectedTarget[i]
+    print(f'{diff}\t{testPrediction[i]} : {expectedTarget[i]}')
+
+print("\n\ntest\n")
+testPrediction = clfSVM.predict(trainFeatures[4*singleGroupLength:])
+expectedValues = trainTarget[4*singleGroupLength:]
+
+for i in range(len(expectedValues)):
+    diff = abs(testPrediction[i] - expectedValues[i])
+    print(f'{diff}\t{testPrediction[i]} : {expectedValues[i]}')
