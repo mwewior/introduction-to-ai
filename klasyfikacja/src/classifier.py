fce0bfec10a5716e1e5a2d544d9a53e145d9c92a
mwewior <01168918@pw.edu.pl>
2024-05-03 16:34:41 +0200

    print functions added

--- klasyfikacja/src/classifier.py
+++ klasyfikacja/src/classifier.py
@@ -35,6 +35,28 @@ def printInfo(clf) -> None:
         info += f'\n     C: {clf.C}, '
         info += f'\n   tol: {clf.tol}, '
         info += f'\n  iter: {clf.max_iter} '
+    info += '\n\n'
+    print(info)
+
+
+def printPredictions(accuracy, errors, predictions, targets):
+    info = ""
+    info += f'\naccuracy = {round(accuracy, DIGITS)}'
+    info += f'\nerrors:    {errors}'
+    info += f'\npredicted: {predictions}'
+    info += f'\ntarget:    {targets}'
+    print(info)
+
+
+def printMetrics(observations: List[Observation]):
+    info = ""
+    for obs in observations:
+        info += f'\nClass: {obs.name}'
+        info += f'\n accuracy: {round(obs.accuracy(), DIGITS)}'
+        info += f'\nprecision: {round(obs.precision(), DIGITS)}'
+        info += f'\n   recall: {round(obs.recall(), DIGITS)}'
+        info += f'\n       F1: {round(obs.F1(), DIGITS)}'
+        info += '\n'
     info += '\n'
     print(info)
 
@@ -62,10 +84,12 @@ clf = clfTree
 printInfo(clf)
 
 
-oSetosa = Observation(name="Setosa")
-oVersicolor = Observation(name="Versicolor")
-oVirginica = Observation(name="Virginica")
-observations = [oSetosa, oVersicolor, oVirginica]
+# oSetosa = Observation(name="Setosa")
+# oVersicolor = Observation(name="Versicolor")
+# oVirginica = Observation(name="Virginica")
+# observations = [oSetosa, oVersicolor, oVirginica]
+
+
 accuracies = []
 
 
@@ -85,12 +109,17 @@ for k in range(FOLDS):
     predict_str = ""
     where_error_str = ""
 
-    for i in range(numerosity):
-        predict_str += f'{testPrediction[i]} '
-        target_str += f'{testTargets[i]} '
+    oSetosa = Observation(name="Setosa")
+    oVersicolor = Observation(name="Versicolor")
+    oVirginica = Observation(name="Virginica")
+    observations = [oSetosa, oVersicolor, oVirginica]
 
+    for i in range(numerosity):
         targetClass = testTargets[i]
         predictClass = testPrediction[i]
+
+        predict_str += f'{predictClass} '
+        target_str += f'{targetClass} '
 
         if (predictClass == targetClass):
             AccuracyPOSITIVE += 1
@@ -104,23 +133,27 @@ for k in range(FOLDS):
     accuracies.append(accuracy)
 
     print(f'Fold {k}: accuracy = {round(accuracy, DIGITS)}')
-    # print(f'Fold {k}: accuracy = {round(accuracy, DIGITS)}')
+    printPredictions(accuracy, where_error_str, predict_str, target_str)
+    # print(f'accuracy = {round(accuracy, DIGITS)}')
     # print(f'errors:    {where_error_str}')
     # print(f'predicted: {predict_str}')
     # print(f'target:    {target_str}\n')
 
+    printMetrics(observations)
 
-for obs in observations:
-    print('')
-    print(f'Class: {obs.name}')
-    print(f' accuracy: {round(obs.accuracy(), DIGITS)}')
-    print(f'precision: {round(obs.precision(), DIGITS)}')
-    print(f'   recall: {round(obs.recall(), DIGITS)}')
-    print(f'       F1: {round(obs.F1(), DIGITS)}')
+
+
+# for obs in observations:
+#     print('')
+#     print(f'Class: {obs.name}')
+#     print(f' accuracy: {round(obs.accuracy(), DIGITS)}')
+#     print(f'precision: {round(obs.precision(), DIGITS)}')
+#     print(f'   recall: {round(obs.recall(), DIGITS)}')
+#     print(f'       F1: {round(obs.F1(), DIGITS)}')
 
 avgAccuracy = np.mean(accuracies)
 stddevAccuracy = np.std(accuracies)
 print(
-    f'\nOverall mean accuracy: {round(avgAccuracy, DIGITS)}\n' +
+    f'Overall mean accuracy: {round(avgAccuracy, DIGITS)}\n' +
     f'standard deviation: {stddevAccuracy}\n'
     )
