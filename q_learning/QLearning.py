aba7c3d78ea5fe26cb9332afc065eafd73a3b909
mwewior <01168918@pw.edu.pl>
2024-05-23 22:54:38 +0200

    first implamentation - it works - not sure if its ideal

---
+++ q_learning/QLearning.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+class Q:
+    def __init__(
+        self,
+        map_shape: tuple[int] = (64, 4),
+        learning_rate: float = 0.1,
+        discount: float = 0.8,
+        policy: str = "Eps-greedy",     # "Boltzman"
+        eps: float = 0.5,
+        T: float = 1.0,
+    ):
+        self.Q = np.zeros(shape=map_shape)
+        self.learning_rate = learning_rate  # beta (or alpha)
+        self.discount = discount            # gamma
+        self.policy = policy
+        self.eps = eps
+        self.T = T
+        self.state = 0
+
+    def epsGreedy(self) -> int:
+        randval = np.random.random()
+        if randval < self.eps:
+            move = np.random.randint(0, 4)
+        else:
+            move = self.Q[self.state, :].argmax()
+        return move
+
+    def boltzman(self) -> int:
+        moves = list(range(4))
+        p = []
+        for m in moves:
+            p.append(np.exp(self.Q[self.state, m] / self.T))
+
+        denominator = sum(p)
+        for i in moves:
+            p[i] = p[i] / denominator
+
+        move = np.random.choice(moves, p=p)
+        return move
+
+    def chooseAction(self) -> int:
+        if self.policy == "Eps-greedy":
+            return self.epsGreedy()
+        if self.policy == "Boltzman":
+            return self.boltzman()
+
+    def updateQ(self, next_state: int, action: int, reward: float) -> None:
+        lr = self.learning_rate
+        gamma = self.discount
+
+        prev = (1 - lr) * self.Q[self.state, action]
+        updated = lr * (reward + gamma * max(self.Q[next_state, :]))
+
+        self.Q[self.state, action] = prev + updated
+        self.state = next_state
---
+++ q_learning/main.py
@@ -0,0 +1,57 @@
+import gym
+import QLearning
+
+
+mapsize = 64
+actionTypes = 4
+QSHAPE = (mapsize, actionTypes)
+
+
+def run(
+    env: gym.Env,
+    Tmax: int = 1000,
+    Q: QLearning.Q = None,
+    policy: str = "Eps-greedy",
+    eps: int = 0.8,
+):
+    if Q is None:
+        Q = QLearning.Q(
+            QSHAPE,
+            learning_rate=0.1,
+            discount=0.87,
+            policy=policy,  # Eps-greedy | Boltzman
+            eps=eps,
+            T=2,
+        )
+
+    # observation, info = env.reset(seed=SEED)
+    observation, info = env.reset()
+
+    for _ in range(Tmax):
+        action = Q.chooseAction()
+
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        Q.updateQ(observation, action, reward)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+    print("\nfinall Q Table:")
+    print(Q.Q)
+    return Q
+
+
+if __name__ == "__main__":
+    env = gym.make(
+        "FrozenLake-v1",
+        desc=None,
+        map_name="8x8",
+        is_slippery=True,
+        # render_mode="human",  # jeżeli ma się wyświetlać
+    )
+    t = int(1e6)
+    Q1 = run(env, Tmax=t, policy="Epsa-greedy", eps=0.9)
+    Q2 = run(env, Tmax=t, Q=Q1, policy="Boltzman")
+    Q3 = run(env, Tmax=t, Q=Q2, policy="Epsa-greedy", eps=0.15)
